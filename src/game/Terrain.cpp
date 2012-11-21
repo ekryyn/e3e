e0dba2d72961b7bfc856dac841a3d906a2a9bf3b
ekryyn <tedefump@gmail.com>
2012-11-21 18:55:21 +0100

    Correct Terrain face culling

--- src/game/Terrain.cpp
+++ src/game/Terrain.cpp
@@ -49,10 +49,10 @@ void Terrain::initFromMap(const Map &map)
 
 			v1.z = 0; v2.z = 0; v3.z = 0; v4.z = 0;
 
-			v1.x = j*cellSize; v1.y = i*cellSize;
-			v2.x = (j+1)*cellSize; v2.y = i*cellSize;
-			v3.x = (j+1)*cellSize; v3.y = (i+1)*cellSize;
-			v4.x = j*cellSize; v4.y = (i+1)*cellSize;
+			v1.x = j*cellSize; v1.y = i*cellSize*-1;
+			v2.x = (j+1)*cellSize; v2.y = i*cellSize*-1;
+			v3.x = (j+1)*cellSize; v3.y = (i+1)*cellSize*-1;
+			v4.x = j*cellSize; v4.y = (i+1)*cellSize*-1;
 
 			uv1.x = (float)j/map.width; uv1.y = 1.f - (float)(i+1)/map.height;
 			uv2.x = (float)(j+1)/map.width; uv2.y = 1.f - (float)(i+1)/map.height;
@@ -60,11 +60,11 @@ void Terrain::initFromMap(const Map &map)
 			uv4.x = (float)j/map.width; uv4.y = 1.f - (float)(i)/map.height;
 
 
-			vertices.push_back(v1); vertices.push_back(v4);
-			vertices.push_back(v3); vertices.push_back(v2);
+			vertices.push_back(v1); vertices.push_back(v2);
+			vertices.push_back(v3); vertices.push_back(v4);
 
-			uvCoords.push_back(uv1); uvCoords.push_back(uv4);
-			uvCoords.push_back(uv3); uvCoords.push_back(uv2);
+			uvCoords.push_back(uv1); uvCoords.push_back(uv2);
+			uvCoords.push_back(uv3); uvCoords.push_back(uv4);
 
 			e3e::Vector3d n(0,0,1);
 			vertexNormals.push_back(n); vertexNormals.push_back(n);
@@ -103,6 +103,7 @@ void Terrain::initFromMap(const Map &map)
 
 	material.diffuse = e3e::Color(1,0,0);
 	normal_state.draw_vertex_normals = true;
+	normal_state.draw_face_normals = true;
 
 	initGeometry(false);
 	initOpenGL();
